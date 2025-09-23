354b824b7bd3a827bf2d651835dc947a46d16998
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2025-09-23 20:45:20 +0600

    commit

--- company/action.py
+++ company/action.py
@@ -35,7 +35,7 @@ def get_company():
     try:
         connection = create_database_connection()
         cursor = connection.cursor()
-        sql_select_Query = "SELECT id, company_name, magicport_url FROM companies_directory WHERE is_active = FALSE AND id >= 12362 ORDER BY id ASC LIMIT 1"
+        sql_select_Query = "SELECT id, company_name, magicport_url FROM companies_directory WHERE is_active = FALSE AND id > 10501 AND id < 12362 ORDER BY id ASC LIMIT 1"
         cursor.execute(sql_select_Query)
         records = cursor.fetchall()
         if connection:
